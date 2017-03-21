556ce593ee91d0ce5ccffd2beb11e6b05915c827
Kolbjørn Austreng <kolbjora@stud.ntnu.no>
2017-03-21 19:34:19 +0100

    Supervisor implemented, echos
    Signed-off-by: Kolbjørn Austreng <kolbjora@stud.ntnu.no>

---
+++ apps/server/lib/server/application.ex
@@ -0,0 +1,17 @@
+defmodule Server.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      supervisor(Task.Supervisor, [[name: Server.TaskSupervisor]]),
+      worker(Task, [Server, :accept, [4040]])
+    ]
+
+    opts = [strategy: :one_for_one, name: Server.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
